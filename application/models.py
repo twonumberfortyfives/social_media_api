97a12400462f16480746921db7b77e70a7e36d67
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-05 01:38:14 +0300

    added Follow model

--- application/models.py
+++ application/models.py
@@ -35,4 +35,7 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 
-
+class Follow(models.Model):
+    follower = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followers_following')
+    following = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followers_following')
+    created_at = models.DateTimeField(auto_now_add=True)
