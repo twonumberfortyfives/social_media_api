86003d75a4ea4540f0b20789fe28a0772bb336c4
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-05 01:31:55 +0300

    implemented image field for user.

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -129,3 +129,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "user.User"
+
+MEDIA_ROOT = BASE_DIR/'media'
+
+MEDIA_URL = '/media/'
--- user/models.py
+++ user/models.py
@@ -1,9 +1,20 @@
+import os
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.text import slugify
+
+
+def user_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/trains/", filename)
 
 
 class User(AbstractUser):
-    profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
+    profile_picture = models.ImageField(upload_to=user_image_file_path, blank=True)
     bio = models.TextField(max_length=500, blank=True)
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=True)
