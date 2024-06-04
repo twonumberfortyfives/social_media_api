bb84b693d88c6a26b2ce941c9c1bda9101f9d38a
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-05 02:28:04 +0300

    simple jwt implementation

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'user',
     'application',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,10 @@ AUTH_USER_MODEL = "user.User"
 MEDIA_ROOT = BASE_DIR/'media'
 
 MEDIA_URL = '/media/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
--- user/urls.py
+++ user/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 
 from user.views import CreateUserView
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 app_name = 'user'
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
