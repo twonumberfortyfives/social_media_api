02f272ee79372325a78a634e6a1fe413851de813
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-05 01:34:57 +0300

    added Post model

--- application/models.py
+++ application/models.py
@@ -1,5 +1,24 @@
+import os
+import uuid
+
 from django.db import models
+from django.utils.text import slugify
+
+from social_media_api.settings import AUTH_USER_MODEL
+
+
+def post_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/trains/", filename)
 
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField(max_length=500)
+    media = models.FileField(upload_to=post_image_file_path, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
