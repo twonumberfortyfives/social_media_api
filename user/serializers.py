a33e44455854b96fa7badf8c513a8bbbea8b914d
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-06 01:17:36 +0300

    code refactoring

--- user/serializers.py
+++ user/serializers.py
@@ -12,7 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
             "id",
             "email",
             "bio",
-            "password",
+            "password",                                                     
             "profile_picture",
             "followings",
             "followers",
